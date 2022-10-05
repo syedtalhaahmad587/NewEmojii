bd7f8158ca36ae2ac95416457b669d1275731e09
syedtalhaahmad587 <syedtalhaahmad587@gmail.com>
2022-10-05 23:44:14 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Data from "./emojiData.json";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+
+
 function App() {
+  
+  const [name, setName] = useState('');
+  console.log(name)
+  const [foundUsers, setFoundUsers] = useState(Data);
+  console.log(foundUsers)
+  const filter = (e) => {
+    const keyword = e.target.value;
+    if (keyword !== '') {
+      const results = Data.filter((user) => {
+        return user.name.toLowerCase().startsWith(keyword.toLowerCase());
+
+      });
+      setFoundUsers(results);
+    } else {
+      setFoundUsers(Data);
+    }
+
+    setName(keyword);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+      className="form-control form-control-lg inputSet"
+        type="search"
+        value={name}
+        onChange={filter}
+        placeholder="Filter"
+      />
+      <div className="container">
+        {foundUsers && foundUsers.length > 0 ? (
+          foundUsers.map((item) => (
+            <div className='reflected'>
+                    <p className='col-lg-3 col-md-4 col-sm-2 col-xs-6' key={item.id}>{item.name}</p>
+        </div>
+          ))
+        ) : (
+          <h1>No results found!</h1>
+        )}
+      </div>
     </div>
   );
 }
